999e570dd9153a7a3c1f5ff6f5e6c014fcdbf298
autumncrypto <autumn0xcryptogy@gmail.com>
2026-02-03 08:11:29 +0000

    Update LibEVM.sol

--- src/utils/LibEVM.sol
+++ src/utils/LibEVM.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import {ACCOUNT_CODE_STORAGE_SYSTEM_CONTRACT} from "era-contracts/system-contracts/Constants.sol";
 import {Utils} from "era-contracts/system-contracts/libraries/Utils.sol";
 
-/// @notice Library for detecting EVM comaptibility of addresses
+/// @notice Library for detecting EVM compatibility of addresses
 /// @author Abstract (https://github.com/Abstract-Foundation/absmate/blob/main/src/utils/LibEVM.sol)
 library LibEVM {
     /// @dev returns true if the address is EVM compatible. Empty addresses are assumed to be EOAs
