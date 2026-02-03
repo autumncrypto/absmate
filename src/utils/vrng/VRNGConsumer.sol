c5c06169ef5828bfb873638d34b671c122ecac2f
autumncrypto <autumn0xcryptogy@gmail.com>
2026-02-03 08:08:33 +0000

    Update VRNGConsumer.sol

--- src/utils/vrng/VRNGConsumer.sol
+++ src/utils/vrng/VRNGConsumer.sol
@@ -6,7 +6,7 @@ import {VRNGConsumerAdvanced} from "./VRNGConsumerAdvanced.sol";
 import {VRNGNormalizationMethod} from "./DataTypes.sol";
 
 /// @title VRNGConsumer
-/// @author Abstract (https://github.com/Abstract-Foundation/absmate/blob/main/src/utils/VRNGConsumer.sol)
+/// @author Abstract (https://github.com/Abstract-Foundation/absmate/blob/main/src/utils/vrng/VRNGConsumer.sol)
 /// @notice A simple consumer contract for requesting randomness from Proof of Play vRNG. (https://docs.proofofplay.com/services/vrng/about)
 /// @dev Must initialize via `_setVRNG` function before requesting randomness.
 abstract contract VRNGConsumer is VRNGConsumerAdvanced {
